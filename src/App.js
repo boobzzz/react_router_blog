2792df6b631d1440f67e74e711dd480c4d4aa7fe
Yuriy Borys <iam618384@gmail.com>
2019-12-20 08:02:43 +0200

    Develop commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 
@@ -8,12 +8,10 @@ export default class App extends Component {
     render() {
 
         return (
-            <Router>
-                <div>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                </div>
-            </Router>
+            <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </div>
         )
     }
 }
